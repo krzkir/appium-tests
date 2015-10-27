3f1b72d49bfd79fabf15fadae455908e4207b0af
krzkir <krzysztof.kiryluk@blstream.com>
2015-10-27 14:16:52 +0100

    reindented

--- appium/android/pages/drawer.rb
+++ appium/android/pages/drawer.rb
@@ -1,29 +1,27 @@
 module Endomondo
-	module Drawer
-		class << self
+  module Drawer
+	class << self
 
-			def assert_exists
-			#	@history = find_element(:xpath,"//*[contains(@text,'History')]")
-				@history = text('History')
-			#	@signup = find_element(:id, 'signup') @class='android.widget.TextView'and 
-			end
+	  def assert_exists
+		@history = text('History')
+	  end
 
-			def assert
-				wait { assert_exists }
-			end
-						
-			def click_history
-				wait { @history.click }
-			end
+	  def assert
+		wait { assert_exists }
+	  end
 
+	  def click_history
+		wait { @history.click }
+	  end
 
-
-		end
 	end
+  end
 end
 
 module Kernel
-	def drawer_page
-		Endomondo::Drawer
-	end
+
+  def drawer_page
+	Endomondo::Drawer
+  end
+  
 end
